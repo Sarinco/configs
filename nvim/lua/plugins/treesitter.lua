03205691da7d2a2307a37a4a262c6968009e8474
Sarinco <deleuxantoine@gmail.com>
2025-03-03 17:38:00 +0100

    addition of recoring message when recording on the lualine

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function () 
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "rust", "python", "java" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "rust", "python", "java", "markdown" },
           highlight = { enable = true },
           indent = { enable = true },  
         })
